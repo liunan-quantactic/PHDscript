2e0a2172008fdf848e3de914c27d053186490ef7
liunan-quantactic <finaholic@163.com>
2022-03-01 18:02:43 +0800

    a little modify

--- chapter7.py
+++ chapter7.py
@@ -5,16 +5,16 @@ import pandas as pd
 import statsmodels.api as sm
 
 #全部数据
-df = pd.read_csv("F:\\all_factor_month.csv",index_col=0,parse_dates=True)
+df = pd.read_csv("G:\\all_factor_month.csv",index_col=0,parse_dates=True)
 #获得目标数据
-need_df=df[["chg","codenum","beta","EBITDA2EV","free_EV","MOMOM","STOM","risk_aver"]]
+need_df=df[["chg","codenum","beta","EBITDA2EV","free_EV","MOMOM","STOM"]]
 need_df=need_df[need_df["codenum"].isna()==False]
 #-------------------求因子之间的相关性---------------------------------
 #按时间排列因子
 period=np.unique(need_df.index)
 mean=[]
 for t in period:
-    mean.append(need_df.loc[t].mean(axis=0))
+    mean.append(need_df[["beta","EBITDA2EV","free_EV","MOMOM","STOM"]].loc[t].mean(axis=0))
 ts_df=pd.DataFrame(mean,index=period)
 ts_df=ts_df.fillna(method='bfill')
 Q=ts_df.corr().values
@@ -32,7 +32,7 @@ for c in all_code:
     if len(temp_df)>=10:
         codes.append(c)
         y=temp_df['chg']
-        x=temp_df[["beta","EBITDA2EV","free_EV","MOMOM","STOM","risk_aver"]]
+        x=temp_df[["beta","EBITDA2EV","free_EV","MOMOM","STOM"]]
         X=sm.add_constant(x,has_constant='add')
         mod=sm.OLS(y,X).fit()
         betas.append(mod.params.values)
@@ -40,8 +40,8 @@ B=pd.DataFrame(betas,index=codes)
 B=B.fillna(method='ffill')
 
 
-XX=pd.DataFrame(betas,index=codes,columns=["const","beta","EBITDA2EV","free_EV","MOMOM","STOM","risk_aver"])
-XXB=XX[["beta","EBITDA2EV","free_EV","MOMOM","STOM","risk_aver"]].values
+XX=pd.DataFrame(betas,index=codes,columns=["const","beta","EBITDA2EV","free_EV","MOMOM","STOM"])
+XXB=XX[["beta","EBITDA2EV","free_EV","MOMOM","STOM"]].values
 XXBQ=np.dot(XXB,Q)
 XXBQB=np.dot(XXBQ,XXB.T)
 XXBQB=matrix(XXBQB,tc="d")
@@ -55,11 +55,12 @@ p_chg_df=p_chg_df[codes].fillna(0)
 #c为这些股票的收益率
 w=[]
 for t in p_chg_df.index:
-    c=matrix(p_chg_df.loc[t],tc='d')
+    c=matrix(-1*p_chg_df.loc[t],tc='d')
     G=matrix(-1*np.diag(np.ones(len(codes))),tc='d')
     h=matrix(np.zeros(len(codes)),tc='d')
     A=matrix(np.ones(len(codes)),tc='d').T
     b=matrix(1,tc='d')
     sol = solvers.qp(XXBQB, c, G, h, A, b)
-    w.append(sol['x'])
+    w.append(list(sol['x']))
 
+W_all_NRA=pd.DataFrame(w,index=p_chg_df.index,columns=codes)
